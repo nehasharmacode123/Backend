299cc7b34cf4f4b9e77995ec5b77832a599cf62e
nehasharmacode123 <nehasharma962595@gmail.com>
2025-11-22 23:02:31 +0530

    Day_4DeleteAPI

--- backend/Day_4/server.js
+++ backend/Day_4/server.js
@@ -25,6 +25,18 @@ server.get("/notes", (req, res) => {
   res.json(notes);
 });
 
+// DELETE /note/:index
+// DELETE /note/:2
+
+server.delete('/notes/:index',(req,res)=>{
+  const index = req.params.index
+  delete notes[index]
+  res.json({
+    message:"note deleted successfully",
+  })
+})
+
+
 server.listen(3000, () => {
   console.log(`Server is running on port 3000`);
 });
